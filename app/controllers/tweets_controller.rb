149a62cc6e091cf8bd1b80e89c6f3f5e1b4397c1
Conor Hamilton <conhamil19@gmail.com>
2018-10-26 19:17:21 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -68,7 +68,6 @@ class TweetsController < ApplicationController
   delete '/tweets/:id/delete' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
-      if @tweet && @tweet.user == current_user
         @tweet.delete
         redirect '/tweets'
       else
