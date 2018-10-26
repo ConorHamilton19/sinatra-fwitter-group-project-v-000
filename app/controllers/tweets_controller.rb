2c0157993e023ff9657e92613ce8c81c12134a29
Conor Hamilton <conhamil19@gmail.com>
2018-10-26 18:06:39 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,13 @@
 class TweetsController < ApplicationController
+  
+  get '/tweets/new' do
+    erb :"/tweets/new"
+  end 
+  
+  get '/tweets' do 
+    @tweets = Tweet.all
+    erb :"/tweets/index"
+  end 
 
 
 end
